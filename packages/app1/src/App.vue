a9941df604b89346f54d208b561ab49e57e9a82b
何添和 <heth@essence.com.cn>
2022-04-04 10:28:09 +0800

    chore: init

---
+++ packages/app1/src/App.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>VITE_APP_API_HOST: {{ aipHost }}</div>
+</template>
+
+<script setup lang="ts">
+const aipHost = import.meta.env.VITE_APP_API_HOST
+
+console.log(aipHost)
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  text-align: center;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
